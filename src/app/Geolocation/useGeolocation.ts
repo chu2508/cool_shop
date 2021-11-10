38e8e3d2355662d9d4b0d05e9d054a0af647904f
zheng.liuli <767848898@qq.com>
2021-11-10 18:28:21 +0800

    ✨ feat: 添加获取附近商店 hook

--- src/app/Geolocation/useGeolocation.ts
+++ src/app/Geolocation/useGeolocation.ts
@@ -1,7 +1,10 @@
 import getGeolocation from "src/tools/getGeolocation";
+import getServiceToken from "src/tools/getServiceToken";
 import useSwr from "swr";
 
 export default function useGeolocation() {
   const { data, error } = useSwr("/app/geolocation", getGeolocation);
   return { geolocation: data, error };
 }
+
+export const GeolocationService = getServiceToken(useGeolocation);
---
+++ src/app/Shop/useShop.ts
@@ -0,0 +1,32 @@
+import { useContext, useMemo } from "react";
+import getDistance from "src/tools/getDistance";
+import useSwr from "swr";
+import { GeolocationService } from "../Geolocation/useGeolocation";
+
+const fetcher = async () => {
+  return {
+    id: "1",
+    name: "光谷壹号店",
+    location: { lon: 35.120011, lat: 24.11251 },
+    service: ["外卖", "预约自提"],
+  };
+};
+
+export default function useShop() {
+  const { geolocation } = useContext(GeolocationService);
+  const { data, error } = useSwr(
+    geolocation ? ["/api/shop", geolocation] : null,
+    fetcher
+  );
+
+  const distance = useMemo(() => {
+    if (geolocation && data) {
+      const { lon: lon1, lat: lat1 } = data.location;
+      const { lon: lon2, lat: lat2 } = geolocation;
+      return getDistance(lon1, lat1, lon2, lat2);
+    }
+    return;
+  }, [geolocation, data]);
+
+  return { error, current: data, distance };
+}
