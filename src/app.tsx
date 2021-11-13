d78dbf4f9aa7a880c1fb17a16e4e566f5e22cc95
dirac <767848898@qq.com>
2021-11-13 22:46:02 +0800

    ✨ feat: 首页预览组件编写情况

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,31 @@
-import { Component } from "react";
+import useGeolocation, {
+  GeolocationService
+} from "@app/Geolocation/useGeolocation";
+import useCurrentShop, { CurrentShopService } from "@app/Shop/useCurrentShop";
+import React, { FC } from "react";
 import "./app.scss";
 
-class App extends Component {
-  componentDidMount() {}
+const ShopServiceWrap = getWrap(CurrentShopService, useCurrentShop);
 
-  componentDidShow() {}
-
-  componentDidHide() {}
-
-  componentDidCatchError() {}
-  render() {
-    return this.props.children;
-  }
-}
-
+const App: FC = (props) => {
+  return (
+    <GeolocationService.Provider value={useGeolocation()}>
+      <ShopServiceWrap>{props.children}</ShopServiceWrap>
+    </GeolocationService.Provider>
+  );
+};
 export default App;
+
+function getWrap<T>(
+  service: React.Context<T>,
+  serviceHook: (...arg: any[]) => T
+) {
+  const Wrapper: FC = (props) => {
+    return (
+      <service.Provider value={serviceHook()}>
+        {props.children}
+      </service.Provider>
+    );
+  };
+  return Wrapper;
+}
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,41 +1,12 @@
-import { Component } from "react";
-import { connect } from "react-redux";
-import { View, Button, Text } from "@tarojs/components";
+import TopShop from "@app/Shop/TopShop";
+import { View } from "@tarojs/components";
 
-import "./index.scss";
+const index = () => {
+  return (
+    <View>
+      <TopShop />
+    </View>
+  );
+};
 
-class Index extends Component {
-  componentWillReceiveProps(nextProps) {
-    console.log(this.props, nextProps);
-  }
-
-  componentWillUnmount() {}
-
-  componentDidShow() {}
-
-  componentDidHide() {}
-
-  render() {
-    return (
-      <View className="index">
-        <Button className="add_btn" onClick={this.props.add}>
-          +
-        </Button>
-        <Button className="dec_btn" onClick={this.props.dec}>
-          -
-        </Button>
-        <Button className="dec_btn" onClick={this.props.asyncAdd}>
-          async
-        </Button>
-        <View>
-          <Text>{this.props.counter.num}</Text>
-        </View>
-        <View>
-          <Text>Hello, World</Text>
-        </View>
-      </View>
-    );
-  }
-}
-
-export default Index;
+export default index;
