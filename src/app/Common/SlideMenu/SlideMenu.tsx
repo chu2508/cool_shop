188c0f415039cae60bd442ea6b074c65e69f6687
dirac <767848898@qq.com>
2021-11-21 15:12:43 +0800

    11

---
+++ src/app/Common/SlideMenu/SlideMenu.tsx
@@ -0,0 +1,7 @@
+import { View } from "@tarojs/components";
+
+const SlideMenu = () => {
+  return <View>slidemenu</View>;
+};
+
+export default SlideMenu;
---
+++ src/app/Common/SlideMenu/useSlideMenu.ts
@@ -0,0 +1,5 @@
+import { useState } from "react";
+
+export default function useSlideMenu() {
+  const [active, setActive] = useState(0);
+}
